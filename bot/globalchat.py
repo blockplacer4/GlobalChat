7b2d984e60fbba7dadbbcb2ca779ff3f9cb74793
LuxxDevs <lkgames256@gmail.com>
2023-12-19 07:04:35 +0100

    basic not functioning global chat bot ;)

--- bot/globalchat.py
+++ bot/globalchat.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import asyncpg
 import aiohttp
 
+
 class GlobalChat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -32,22 +33,27 @@ class GlobalChat(commands.Cog):
             if message.channel.id == record["channel_id"]:
                 webhook = await self.get_webhook(record["channel_id"])
                 if webhook:
-                    async with self.webhook_session.post(webhook.url, json={"content": message.content, "username": message.author.name, "avatar_url": message.author.avatar_url}) as response:
+                    async with self.webhook_session.post(webhook.url, json={"content": message.content,
+                                                                            "username": message.author.name,
+                                                                            "avatar_url": message.author.avatar_url}) as response:
                         pass
 
     @commands.command()
     @commands.has_permissions(manage_webhooks=True)
     async def addglobal(self, ctx, channel: discord.TextChannel):
         async with self.bot.pool.acquire() as conn:
-            await conn.execute("INSERT INTO global_channels (guild_id, channel_id) VALUES ($1, $2)", ctx.guild.id, channel.id)
+            await conn.execute("INSERT INTO global_channels (guild_id, channel_id) VALUES ($1, $2)", ctx.guild.id,
+                               channel.id)
         await ctx.send(f"{channel.mention} wurde zum Global Chat hinzugefügt.")
 
     @commands.command()
     @commands.has_permissions(manage_webhooks=True)
     async def removeglobal(self, ctx, channel: discord.TextChannel):
         async with self.bot.pool.acquire() as conn:
-            await conn.execute("DELETE FROM global_channels WHERE guild_id = $1 AND channel_id = $2", ctx.guild.id, channel.id)
+            await conn.execute("DELETE FROM global_channels WHERE guild_id = $1 AND channel_id = $2", ctx.guild.id,
+                               channel.id)
         await ctx.send(f"{channel.mention} wurde vom Global Chat entfernt.")
+
 
 def setup(bot):
     bot.add_cog(GlobalChat(bot))
